75d411f126542fab60eefe267a8e7486547a6bae
Tamas Frei <frei.tamas555@gmail.com>
2019-02-27 11:11:44 +0100

    Move MyHandler class to MyHandler.java

--- src/main/java/hu/codecool/annotation_assignment/Test.java
+++ src/main/java/hu/codecool/annotation_assignment/Test.java
@@ -1,11 +1,7 @@
 package hu.codecool.annotation_assignment;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class Test {
@@ -15,17 +11,5 @@ public class Test {
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
-    }
-
-    static class MyHandler implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
     }
 }
