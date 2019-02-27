08bb98bd8a71aa2fab3395b9d471f731740f8d60
Tamas Frei <frei.tamas555@gmail.com>
2019-02-27 12:37:36 +0100

    Add method to WebRoutes

--- src/main/java/hu/codecool/annotation_assignment/Routes.java
+++ src/main/java/hu/codecool/annotation_assignment/Routes.java
@@ -2,12 +2,12 @@ package hu.codecool.annotation_assignment;
 
 public class Routes {
 
-    @WebRoute(path = "/test")
+    @WebRoute(path = "/test", method = HttpRequestMethod.GET)
     public String test1() {
         return "This is the fist test route";
     }
 
-    @WebRoute(path = "/another-test")
+    @WebRoute(path = "/another-test", method = HttpRequestMethod.GET)
     public String test2() {
         return "This is another test route";
     }
