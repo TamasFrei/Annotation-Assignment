daf7f4d1c0017884d53e6fe1ce789692aa4090fd
Tamas Frei <frei.tamas555@gmail.com>
2019-02-27 11:22:34 +0100

    Add Routes class with /test route

--- src/main/java/hu/codecool/annotation_assignment/Routes.java
+++ src/main/java/hu/codecool/annotation_assignment/Routes.java
@@ -1,4 +1,10 @@
 package hu.codecool.annotation_assignment;
 
 public class Routes {
+
+    @WebRoute(path = "/test")
+    public String test1() {
+        return "This is the fist test route";
+    }
+
 }
